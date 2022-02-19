c725ee91567ffa1aa3fb6c9a22dbf33d4f268d0e
Taimor <mtaimorkhan@gmail.com>
2022-02-19 15:27:38 -0500

    checking updates

--- App.js
+++ App.js
@@ -2,11 +2,28 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './components/Home.js';
+import { useEffect } from 'react';
 import Settings from './components/Settings.js';
+import * as Location from 'expo-location';
 
 
 export default function App() {
   
+  useEffect(() => {
+    getLocation();
+  });
+
+  const getLocation = async () => {
+    let { status } = await Location.getBackgroundPermissionsAsync();
+    if (status !== 'granted') {
+      console.log('Permission Denied');
+    } else {
+      let currLoc = await Location.getCurrentPositionAsync();
+      console.log(currLoc);
+    }
+
+  }
+
   const Stack = createNativeStackNavigator();
 
   return (
